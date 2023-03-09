27ff0e8df8182aa2dfde180abae26d1c37922d0a
ITfervor <dlwjdduq0@gmail.com>
2023-03-09 21:38:57 +0900

    hong board

--- spring/hongboard/src/main/java/com/study/hongboard/entity/Article.java
+++ spring/hongboard/src/main/java/com/study/hongboard/entity/Article.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @Column
     private String title;
