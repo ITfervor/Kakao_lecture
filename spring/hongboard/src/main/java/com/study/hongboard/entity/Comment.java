c75ed54d0fbc898aab14334109df85a6312ef48f
ITfervor <dlwjdduq0@gmail.com>
2023-03-11 23:07:09 +0900

    hong board

---
+++ spring/hongboard/src/main/java/com/study/hongboard/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.study.hongboard.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+
+}
